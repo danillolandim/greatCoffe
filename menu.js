8011293c0f39f7ed290c250b5da02473768861fa
Danillo Landim <danillozinho@gmail.com>
2022-10-01 00:00:39 -0300

    alteracoes no mobile

--- menu.js
+++ menu.js
@@ -3,6 +3,8 @@ let btnMobile = document.getElementById('btn-mobile')
 function toggleMenu() {
   let nav = document.getElementById('nav')
   nav.classList.toggle('active')
+  let btn = document.getElementById('btn-mobile')
+  btn.classList.toggle('active')
 }
 
 btnMobile.addEventListener('click', toggleMenu)
